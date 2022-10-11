0664aa59a8965b3de634937823de20adc704594f
Henrik Meyer <henrik.meyer1998@gmx.de>
2022-10-11 22:45:04 +0200

    Added Radio

---
+++ VRBikeShop/VRBikeShop/Assets/Scripts/RotateReticle_1.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateReticle_1 : MonoBehaviour
+{
+    public float rotationSpeed;
+    
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0, rotationSpeed * Time.deltaTime, 0);
+    }
+}
