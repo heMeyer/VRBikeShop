2742f9e0ede5a7d12f8c1eb2ff20963dc5a53f54
Henrik Meyer <henrik.meyer1998@gmx.de>
2022-10-25 21:00:48 +0200

    Loading of different Scenes via Script, Fixed Interaction with Radio

---
+++ VRBikeShop/build_BackUpThisFolder_ButDontShipItWithYourGame/il2cppOutput/Assembly-CSharp_CodeGen.c
@@ -0,0 +1,277 @@
+﻿#include "pch-c.h"
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+// 0x00000001 System.Void Flashlight_PRO::Start()
+extern void Flashlight_PRO_Start_m558A327675F2795D930C7F3E7E4C02BC18D02C23 (void);
+// 0x00000002 System.Void Flashlight_PRO::Change_Intensivity(System.Single)
+extern void Flashlight_PRO_Change_Intensivity_m993CBBDFE3AD3F01F3FE14D87431F4596917DADD (void);
+// 0x00000003 System.Void Flashlight_PRO::Switch()
+extern void Flashlight_PRO_Switch_m8B6093CA5ABD005F50EAE05C8E7753788469496F (void);
+// 0x00000004 System.Void Flashlight_PRO::Enable_Particles(System.Boolean)
+extern void Flashlight_PRO_Enable_Particles_mF1C02DF3EC21F06BBB14A5C0AEF556B9A8A75A41 (void);
+// 0x00000005 System.Void Flashlight_PRO::.ctor()
+extern void Flashlight_PRO__ctor_mBDCA88F91E918109D2D80D062F34E4CA01A08708 (void);
+// 0x00000006 System.Void ScenePartLoader::Start()
+extern void ScenePartLoader_Start_m58BCFF29F5E78088B993B2E87AE2736C45877576 (void);
+// 0x00000007 System.Void ScenePartLoader::Update()
+extern void ScenePartLoader_Update_m192661CC6979639CDA6D712A3688BFBFC5F3E6F3 (void);
+// 0x00000008 System.Void ScenePartLoader::OnTriggerEnter(UnityEngine.Collider)
+extern void ScenePartLoader_OnTriggerEnter_m995E4DF9E8ACE0D49CE0473FB958DDC96EC50355 (void);
+// 0x00000009 System.Void ScenePartLoader::OnTriggerExit(UnityEngine.Collider)
+extern void ScenePartLoader_OnTriggerExit_mA8B9A3DEAEC2C10E77C61FF499B2160EEB1D4F18 (void);
+// 0x0000000A System.Void ScenePartLoader::TriggerCheck()
+extern void ScenePartLoader_TriggerCheck_mCA880A325A180AE7BC73A4D8018C47B7F63222CA (void);
+// 0x0000000B System.Void ScenePartLoader::LoadScene()
+extern void ScenePartLoader_LoadScene_mD9A794958F5025CA8DCAA985A3507B8458375C3D (void);
+// 0x0000000C System.Void ScenePartLoader::UnloadScene()
+extern void ScenePartLoader_UnloadScene_m2F42269B460C02C278F5F26F6178978A00D56C01 (void);
+// 0x0000000D System.Void ScenePartLoader::.ctor()
+extern void ScenePartLoader__ctor_m1FD55FA917C37BFAEF269E71000BE8CA77EE61F4 (void);
+// 0x0000000E System.Void BouncyBallControl::Start()
+extern void BouncyBallControl_Start_m908ABEC7D4D1DEC4A5078A8C63B6B402A3ADB777 (void);
+// 0x0000000F System.Void BouncyBallControl::Update()
+extern void BouncyBallControl_Update_m7EB24EE707731D7C2A1ABE2890E86A37A3BC7AA3 (void);
+// 0x00000010 System.Void BouncyBallControl::.ctor()
+extern void BouncyBallControl__ctor_mAAAD74D7AD21417F142CAE0A8A1145065B3C9989 (void);
+// 0x00000011 System.Void PlayContinuousSound::Awake()
+extern void PlayContinuousSound_Awake_mC24BAD8E0F9890D2BBC8F17D0DA13EF36451553C (void);
+// 0x00000012 System.Void PlayContinuousSound::Start()
+extern void PlayContinuousSound_Start_mE9004587F3089646A75BD1AA14FD7F1D927E9DD5 (void);
+// 0x00000013 System.Void PlayContinuousSound::Play(UnityEngine.AudioClip)
+extern void PlayContinuousSound_Play_m8AFB5CBB5D9C7A576322AE6EF36AEECAF976E1C7 (void);
+// 0x00000014 System.Void PlayContinuousSound::Pause()
+extern void PlayContinuousSound_Pause_mB41628A030E1BE7B30A83EAF9EF58B8BA1DF2C0C (void);
+// 0x00000015 System.Void PlayContinuousSound::TogglePlay()
+extern void PlayContinuousSound_TogglePlay_m50C6CBD99C2D853D844CE7E92EA207561FF2B8AA (void);
+// 0x00000016 System.Void PlayContinuousSound::SetPlay(System.Boolean)
+extern void PlayContinuousSound_SetPlay_m376F3113B0BCF6EE9F1114CC3D13376C4116E745 (void);
+// 0x00000017 System.Collections.IEnumerator PlayContinuousSound::wait()
+extern void PlayContinuousSound_wait_mECBC31FBDC9ED5A9A7BF949EF72598BBF7579596 (void);
+// 0x00000018 System.Boolean PlayContinuousSound::IsPlaying()
+extern void PlayContinuousSound_IsPlaying_mF9BCCFF25ABB399CF1B003F7E145134617B58279 (void);
+// 0x00000019 System.Void PlayContinuousSound::SetClip(UnityEngine.AudioClip)
+extern void PlayContinuousSound_SetClip_m2B4DA98DFAAC392B4806BF87CD7B9A8A0D908E32 (void);
+// 0x0000001A System.Void PlayContinuousSound::OnValidate()
+extern void PlayContinuousSound_OnValidate_m8BFC33CC7DBCB27BCD603C5B56DD66023F90D7D0 (void);
+// 0x0000001B System.Void PlayContinuousSound::.ctor()
+extern void PlayContinuousSound__ctor_mD5399E7D3E20450A5D56E33FE4A1915211651A5F (void);
+// 0x0000001C System.Void PlayContinuousSound/<wait>d__12::.ctor(System.Int32)
+extern void U3CwaitU3Ed__12__ctor_m48A2AF62D8F175C21DB6F257937E6471ADDEF1CE (void);
+// 0x0000001D System.Void PlayContinuousSound/<wait>d__12::System.IDisposable.Dispose()
+extern void U3CwaitU3Ed__12_System_IDisposable_Dispose_m9AF3DF0720892DC7E6AB6D875B05A60DC1ED386E (void);
+// 0x0000001E System.Boolean PlayContinuousSound/<wait>d__12::MoveNext()
+extern void U3CwaitU3Ed__12_MoveNext_mA05DF74DBF202A4E59A527E0D5A653D1C148A5CC (void);
+// 0x0000001F System.Object PlayContinuousSound/<wait>d__12::System.Collections.Generic.IEnumerator<System.Object>.get_Current()
+extern void U3CwaitU3Ed__12_System_Collections_Generic_IEnumeratorU3CSystem_ObjectU3E_get_Current_m92888B531A35D253AAA23EB087EA9C192522E1B4 (void);
+// 0x00000020 System.Void PlayContinuousSound/<wait>d__12::System.Collections.IEnumerator.Reset()
+extern void U3CwaitU3Ed__12_System_Collections_IEnumerator_Reset_m85DF8926C78F62E0CA08B90DF8E3A114DEC292BD (void);
+// 0x00000021 System.Object PlayContinuousSound/<wait>d__12::System.Collections.IEnumerator.get_Current()
+extern void U3CwaitU3Ed__12_System_Collections_IEnumerator_get_Current_m7396BD51194F46AE7535A33A098ED786B2629013 (void);
+// 0x00000022 System.Void radioController::Start()
+extern void radioController_Start_mCAE532E8BD7E277C8A32A10499A9A242DA95892A (void);
+// 0x00000023 System.Void radioController::Update()
+extern void radioController_Update_m4CE55BAD5DEF4B142A32B023E8C706B4A92E9752 (void);
+// 0x00000024 System.Void radioController::toggleMute()
+extern void radioController_toggleMute_m7F6B6526B9E2C0DE6C9289BBF19E3C078B6EDEFE (void);
+// 0x00000025 System.Void radioController::Play(UnityEngine.AudioClip)
+extern void radioController_Play_m3DD35D925CD9EBDDC1E89C27C19C51B2B695F8C4 (void);
+// 0x00000026 System.Void radioController::Pause()
+extern void radioController_Pause_m4C1B20E6C75EE454E37A58C9B25AA51AF44E3A04 (void);
+// 0x00000027 System.Collections.IEnumerator radioController::SwitchOn()
+extern void radioController_SwitchOn_m2776BF17E5ADE8B5B082C3CA321C5029C8D73936 (void);
+// 0x00000028 System.Collections.IEnumerator radioController::SwitchOff()
+extern void radioController_SwitchOff_mA02AFE2EC11101BA5BAD31A1F4403677616E0C81 (void);
+// 0x00000029 System.Void radioController::.ctor()
+extern void radioController__ctor_m69C8ADB16B43E22C8D3C8D356DFF05F7509759AA (void);
+// 0x0000002A System.Void radioController/<SwitchOn>d__12::.ctor(System.Int32)
+extern void U3CSwitchOnU3Ed__12__ctor_m6AA7F7FFDBEDD2DD9E46ED4244CBEE50686111AB (void);
+// 0x0000002B System.Void radioController/<SwitchOn>d__12::System.IDisposable.Dispose()
+extern void U3CSwitchOnU3Ed__12_System_IDisposable_Dispose_m4FE895938A61DB4D70E808AC6C72A32EF5966BB5 (void);
+// 0x0000002C System.Boolean radioController/<SwitchOn>d__12::MoveNext()
+extern void U3CSwitchOnU3Ed__12_MoveNext_mBCE7E9C7CB37F2660927D43B974FD776747F0251 (void);
+// 0x0000002D System.Object radioController/<SwitchOn>d__12::System.Collections.Generic.IEnumerator<System.Object>.get_Current()
+extern void U3CSwitchOnU3Ed__12_System_Collections_Generic_IEnumeratorU3CSystem_ObjectU3E_get_Current_mC0C80A35709BAC85ECA17994A4BD35C2E5BC3B56 (void);
+// 0x0000002E System.Void radioController/<SwitchOn>d__12::System.Collections.IEnumerator.Reset()
+extern void U3CSwitchOnU3Ed__12_System_Collections_IEnumerator_Reset_m976BF7BC4D92276B0E2EBA1F27F46F270D1D3F3D (void);
+// 0x0000002F System.Object radioController/<SwitchOn>d__12::System.Collections.IEnumerator.get_Current()
+extern void U3CSwitchOnU3Ed__12_System_Collections_IEnumerator_get_Current_mB3D9C9234B67CBA6429DBEBF48DBC51D7AAFDB21 (void);
+// 0x00000030 System.Void radioController/<SwitchOff>d__13::.ctor(System.Int32)
+extern void U3CSwitchOffU3Ed__13__ctor_m3C338B9C233D6866E1A070032494FC4888420809 (void);
+// 0x00000031 System.Void radioController/<SwitchOff>d__13::System.IDisposable.Dispose()
+extern void U3CSwitchOffU3Ed__13_System_IDisposable_Dispose_mBF3604F3B41149A96FA1E73EA01051173F160CDC (void);
+// 0x00000032 System.Boolean radioController/<SwitchOff>d__13::MoveNext()
+extern void U3CSwitchOffU3Ed__13_MoveNext_m28E6399136BBCC7E4A837D61F34593A658F1FAC0 (void);
+// 0x00000033 System.Object radioController/<SwitchOff>d__13::System.Collections.Generic.IEnumerator<System.Object>.get_Current()
+extern void U3CSwitchOffU3Ed__13_System_Collections_Generic_IEnumeratorU3CSystem_ObjectU3E_get_Current_m884A9EA0B1CD7094D61B3D00E126E83BC3B33DBB (void);
+// 0x00000034 System.Void radioController/<SwitchOff>d__13::System.Collections.IEnumerator.Reset()
+extern void U3CSwitchOffU3Ed__13_System_Collections_IEnumerator_Reset_mBF7AAE32FF9787CAC1E3A266465815922DD238A7 (void);
+// 0x00000035 System.Object radioController/<SwitchOff>d__13::System.Collections.IEnumerator.get_Current()
+extern void U3CSwitchOffU3Ed__13_System_Collections_IEnumerator_get_Current_m92FE19B53A0084B034AFF4FE284C5E248AF59BB1 (void);
+// 0x00000036 System.Void RotateReticle::Start()
+extern void RotateReticle_Start_m0D4A75096513A7CE2D027A2B98274E1C33ECE6EC (void);
+// 0x00000037 System.Void RotateReticle::Update()
+extern void RotateReticle_Update_m22E59EEAE22F9471ACE5B200B804DF71D86611C1 (void);
+// 0x00000038 System.Void RotateReticle::.ctor()
+extern void RotateReticle__ctor_m188A2931D5FB195EB258B4E140C77B1B82815229 (void);
+// 0x00000039 System.Void RotateReticle_1::Start()
+extern void RotateReticle_1_Start_m05FD8F22CDAA0CF1FA930024A7BC16E5A8CB2A57 (void);
+// 0x0000003A System.Void RotateReticle_1::Update()
+extern void RotateReticle_1_Update_m6022A9BAC88E535921E4C057D717C9D771BCFB57 (void);
+// 0x0000003B System.Void RotateReticle_1::.ctor()
+extern void RotateReticle_1__ctor_m9471C54673ECF478F849F34A79EF359EFD8CF1E8 (void);
+static Il2CppMethodPointer s_methodPointers[59] = 
+{
+	Flashlight_PRO_Start_m558A327675F2795D930C7F3E7E4C02BC18D02C23,
+	Flashlight_PRO_Change_Intensivity_m993CBBDFE3AD3F01F3FE14D87431F4596917DADD,
+	Flashlight_PRO_Switch_m8B6093CA5ABD005F50EAE05C8E7753788469496F,
+	Flashlight_PRO_Enable_Particles_mF1C02DF3EC21F06BBB14A5C0AEF556B9A8A75A41,
+	Flashlight_PRO__ctor_mBDCA88F91E918109D2D80D062F34E4CA01A08708,
+	ScenePartLoader_Start_m58BCFF29F5E78088B993B2E87AE2736C45877576,
+	ScenePartLoader_Update_m192661CC6979639CDA6D712A3688BFBFC5F3E6F3,
+	ScenePartLoader_OnTriggerEnter_m995E4DF9E8ACE0D49CE0473FB958DDC96EC50355,
+	ScenePartLoader_OnTriggerExit_mA8B9A3DEAEC2C10E77C61FF499B2160EEB1D4F18,
+	ScenePartLoader_TriggerCheck_mCA880A325A180AE7BC73A4D8018C47B7F63222CA,
+	ScenePartLoader_LoadScene_mD9A794958F5025CA8DCAA985A3507B8458375C3D,
+	ScenePartLoader_UnloadScene_m2F42269B460C02C278F5F26F6178978A00D56C01,
+	ScenePartLoader__ctor_m1FD55FA917C37BFAEF269E71000BE8CA77EE61F4,
+	BouncyBallControl_Start_m908ABEC7D4D1DEC4A5078A8C63B6B402A3ADB777,
+	BouncyBallControl_Update_m7EB24EE707731D7C2A1ABE2890E86A37A3BC7AA3,
+	BouncyBallControl__ctor_mAAAD74D7AD21417F142CAE0A8A1145065B3C9989,
+	PlayContinuousSound_Awake_mC24BAD8E0F9890D2BBC8F17D0DA13EF36451553C,
+	PlayContinuousSound_Start_mE9004587F3089646A75BD1AA14FD7F1D927E9DD5,
+	PlayContinuousSound_Play_m8AFB5CBB5D9C7A576322AE6EF36AEECAF976E1C7,
+	PlayContinuousSound_Pause_mB41628A030E1BE7B30A83EAF9EF58B8BA1DF2C0C,
+	PlayContinuousSound_TogglePlay_m50C6CBD99C2D853D844CE7E92EA207561FF2B8AA,
+	PlayContinuousSound_SetPlay_m376F3113B0BCF6EE9F1114CC3D13376C4116E745,
+	PlayContinuousSound_wait_mECBC31FBDC9ED5A9A7BF949EF72598BBF7579596,
+	PlayContinuousSound_IsPlaying_mF9BCCFF25ABB399CF1B003F7E145134617B58279,
+	PlayContinuousSound_SetClip_m2B4DA98DFAAC392B4806BF87CD7B9A8A0D908E32,
+	PlayContinuousSound_OnValidate_m8BFC33CC7DBCB27BCD603C5B56DD66023F90D7D0,
+	PlayContinuousSound__ctor_mD5399E7D3E20450A5D56E33FE4A1915211651A5F,
+	U3CwaitU3Ed__12__ctor_m48A2AF62D8F175C21DB6F257937E6471ADDEF1CE,
+	U3CwaitU3Ed__12_System_IDisposable_Dispose_m9AF3DF0720892DC7E6AB6D875B05A60DC1ED386E,
+	U3CwaitU3Ed__12_MoveNext_mA05DF74DBF202A4E59A527E0D5A653D1C148A5CC,
+	U3CwaitU3Ed__12_System_Collections_Generic_IEnumeratorU3CSystem_ObjectU3E_get_Current_m92888B531A35D253AAA23EB087EA9C192522E1B4,
+	U3CwaitU3Ed__12_System_Collections_IEnumerator_Reset_m85DF8926C78F62E0CA08B90DF8E3A114DEC292BD,
+	U3CwaitU3Ed__12_System_Collections_IEnumerator_get_Current_m7396BD51194F46AE7535A33A098ED786B2629013,
+	radioController_Start_mCAE532E8BD7E277C8A32A10499A9A242DA95892A,
+	radioController_Update_m4CE55BAD5DEF4B142A32B023E8C706B4A92E9752,
+	radioController_toggleMute_m7F6B6526B9E2C0DE6C9289BBF19E3C078B6EDEFE,
+	radioController_Play_m3DD35D925CD9EBDDC1E89C27C19C51B2B695F8C4,
+	radioController_Pause_m4C1B20E6C75EE454E37A58C9B25AA51AF44E3A04,
+	radioController_SwitchOn_m2776BF17E5ADE8B5B082C3CA321C5029C8D73936,
+	radioController_SwitchOff_mA02AFE2EC11101BA5BAD31A1F4403677616E0C81,
+	radioController__ctor_m69C8ADB16B43E22C8D3C8D356DFF05F7509759AA,
+	U3CSwitchOnU3Ed__12__ctor_m6AA7F7FFDBEDD2DD9E46ED4244CBEE50686111AB,
+	U3CSwitchOnU3Ed__12_System_IDisposable_Dispose_m4FE895938A61DB4D70E808AC6C72A32EF5966BB5,
+	U3CSwitchOnU3Ed__12_MoveNext_mBCE7E9C7CB37F2660927D43B974FD776747F0251,
+	U3CSwitchOnU3Ed__12_System_Collections_Generic_IEnumeratorU3CSystem_ObjectU3E_get_Current_mC0C80A35709BAC85ECA17994A4BD35C2E5BC3B56,
+	U3CSwitchOnU3Ed__12_System_Collections_IEnumerator_Reset_m976BF7BC4D92276B0E2EBA1F27F46F270D1D3F3D,
+	U3CSwitchOnU3Ed__12_System_Collections_IEnumerator_get_Current_mB3D9C9234B67CBA6429DBEBF48DBC51D7AAFDB21,
+	U3CSwitchOffU3Ed__13__ctor_m3C338B9C233D6866E1A070032494FC4888420809,
+	U3CSwitchOffU3Ed__13_System_IDisposable_Dispose_mBF3604F3B41149A96FA1E73EA01051173F160CDC,
+	U3CSwitchOffU3Ed__13_MoveNext_m28E6399136BBCC7E4A837D61F34593A658F1FAC0,
+	U3CSwitchOffU3Ed__13_System_Collections_Generic_IEnumeratorU3CSystem_ObjectU3E_get_Current_m884A9EA0B1CD7094D61B3D00E126E83BC3B33DBB,
+	U3CSwitchOffU3Ed__13_System_Collections_IEnumerator_Reset_mBF7AAE32FF9787CAC1E3A266465815922DD238A7,
+	U3CSwitchOffU3Ed__13_System_Collections_IEnumerator_get_Current_m92FE19B53A0084B034AFF4FE284C5E248AF59BB1,
+	RotateReticle_Start_m0D4A75096513A7CE2D027A2B98274E1C33ECE6EC,
+	RotateReticle_Update_m22E59EEAE22F9471ACE5B200B804DF71D86611C1,
+	RotateReticle__ctor_m188A2931D5FB195EB258B4E140C77B1B82815229,
+	RotateReticle_1_Start_m05FD8F22CDAA0CF1FA930024A7BC16E5A8CB2A57,
+	RotateReticle_1_Update_m6022A9BAC88E535921E4C057D717C9D771BCFB57,
+	RotateReticle_1__ctor_m9471C54673ECF478F849F34A79EF359EFD8CF1E8,
+};
+static const int32_t s_InvokerIndices[59] = 
+{
+	4507,
+	3603,
+	4507,
+	3491,
+	4507,
+	4507,
+	4507,
+	3570,
+	3570,
+	4507,
+	4507,
+	4507,
+	4507,
+	4507,
+	4507,
+	4507,
+	4507,
+	4507,
+	3570,
+	4507,
+	4507,
+	3491,
+	4413,
+	4329,
+	3570,
+	4507,
+	4507,
+	3547,
+	4507,
+	4329,
+	4413,
+	4507,
+	4413,
+	4507,
+	4507,
+	4507,
+	3570,
+	4507,
+	4413,
+	4413,
+	4507,
+	3547,
+	4507,
+	4329,
+	4413,
+	4507,
+	4413,
+	3547,
+	4507,
+	4329,
+	4413,
+	4507,
+	4413,
+	4507,
+	4507,
+	4507,
+	4507,
+	4507,
+	4507,
+};
+IL2CPP_EXTERN_C const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule;
+const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule = 
+{
+	"Assembly-CSharp.dll",
+	59,
+	s_methodPointers,
+	0,
+	NULL,
+	s_InvokerIndices,
+	0,
+	NULL,
+	0,
+	NULL,
+	0,
+	NULL,
+	NULL,
+	NULL, // module initializer,
+	NULL,
+	NULL,
+	NULL,
+};
